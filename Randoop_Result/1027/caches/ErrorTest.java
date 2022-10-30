205b75a2bc88be7958656fde066f8ed3d7f04c15
Mercy7777777 <54562402+Mercy7777777@users.noreply.github.com>
2022-10-30 02:39:34 -0500

    Add Test classes.

---
+++ Randoop_Result/1027/caches/ErrorTest.java
@@ -0,0 +1,10 @@
+package caches;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ ErrorTest0.class })
+public class ErrorTest {
+}
+
