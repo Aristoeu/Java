dfeceb518d2361c6238c5911381794a5aa4da929
Mercy7777777 <54562402+Mercy7777777@users.noreply.github.com>
2022-11-11 22:29:58 -0600

    Added some tests.

---
+++ Randoop_Result/1111/maths/PrimeFactorization/RegressionTest0.java
@@ -0,0 +1,220 @@
+package maths.PrimeFactorization;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class RegressionTest0 {
+
+    public static boolean debug = false;
+
+    @Test
+    public void test01() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test01");
+        java.util.List<java.lang.Integer> intList1 = com.thealgorithms.maths.PrimeFactorization.pfactors((-1));
+        java.lang.Class<?> wildcardClass2 = intList1.getClass();
+        org.junit.Assert.assertNotNull(intList1);
+        org.junit.Assert.assertNotNull(wildcardClass2);
+    }
+
+    @Test
+    public void test02() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test02");
+        com.thealgorithms.maths.PrimeFactorization primeFactorization0 = new com.thealgorithms.maths.PrimeFactorization();
+        java.lang.Class<?> wildcardClass1 = primeFactorization0.getClass();
+        org.junit.Assert.assertNotNull(wildcardClass1);
+    }
+
+    @Test
+    public void test03() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test03");
+        java.lang.Object obj0 = new java.lang.Object();
+        java.lang.Class<?> wildcardClass1 = obj0.getClass();
+        org.junit.Assert.assertNotNull(wildcardClass1);
+    }
+
+    @Test
+    public void test04() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test04");
+        java.util.List<java.lang.Integer> intList1 = com.thealgorithms.maths.PrimeFactorization.pfactors((int) (byte) 0);
+        java.lang.Class<?> wildcardClass2 = intList1.getClass();
+        org.junit.Assert.assertNotNull(intList1);
+        org.junit.Assert.assertNotNull(wildcardClass2);
+    }
+
+    @Test
+    public void test05() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test05");
+        java.util.List<java.lang.Integer> intList1 = com.thealgorithms.maths.PrimeFactorization.pfactors(1);
+        java.lang.Class<?> wildcardClass2 = intList1.getClass();
+        org.junit.Assert.assertNotNull(intList1);
+        org.junit.Assert.assertNotNull(wildcardClass2);
+    }
+
+    @Test
+    public void test06() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test06");
+        java.util.List<java.lang.Integer> intList1 = com.thealgorithms.maths.PrimeFactorization.pfactors((int) '#');
+        java.lang.Class<?> wildcardClass2 = intList1.getClass();
+        org.junit.Assert.assertNotNull(intList1);
+        org.junit.Assert.assertNotNull(wildcardClass2);
+    }
+
+    @Test
+    public void test07() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test07");
+        java.util.List<java.lang.Integer> intList1 = com.thealgorithms.maths.PrimeFactorization.pfactors((int) (short) 1);
+        java.lang.Class<?> wildcardClass2 = intList1.getClass();
+        org.junit.Assert.assertNotNull(intList1);
+        org.junit.Assert.assertNotNull(wildcardClass2);
+    }
+
+    @Test
+    public void test08() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test08");
+        java.util.List<java.lang.Integer> intList1 = com.thealgorithms.maths.PrimeFactorization.pfactors(10);
+        java.lang.Class<?> wildcardClass2 = intList1.getClass();
+        org.junit.Assert.assertNotNull(intList1);
+        org.junit.Assert.assertNotNull(wildcardClass2);
+    }
+
+    @Test
+    public void test09() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test09");
+        java.util.List<java.lang.Integer> intList1 = com.thealgorithms.maths.PrimeFactorization.pfactors((int) (byte) 100);
+        java.lang.Class<?> wildcardClass2 = intList1.getClass();
+        org.junit.Assert.assertNotNull(intList1);
+        org.junit.Assert.assertNotNull(wildcardClass2);
+    }
+
+    @Test
+    public void test10() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test10");
+        java.util.List<java.lang.Integer> intList1 = com.thealgorithms.maths.PrimeFactorization.pfactors((int) '4');
+        java.lang.Class<?> wildcardClass2 = intList1.getClass();
+        org.junit.Assert.assertNotNull(intList1);
+        org.junit.Assert.assertNotNull(wildcardClass2);
+    }
+
+    @Test
+    public void test11() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test11");
+        java.util.List<java.lang.Integer> intList1 = com.thealgorithms.maths.PrimeFactorization.pfactors((int) 'a');
+        java.lang.Class<?> wildcardClass2 = intList1.getClass();
+        org.junit.Assert.assertNotNull(intList1);
+        org.junit.Assert.assertNotNull(wildcardClass2);
+    }
+
+    @Test
+    public void test12() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test12");
+        java.util.List<java.lang.Integer> intList1 = com.thealgorithms.maths.PrimeFactorization.pfactors((int) (byte) 10);
+        java.lang.Class<?> wildcardClass2 = intList1.getClass();
+        org.junit.Assert.assertNotNull(intList1);
+        org.junit.Assert.assertNotNull(wildcardClass2);
+    }
+
+    @Test
+    public void test13() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test13");
+        java.util.List<java.lang.Integer> intList1 = com.thealgorithms.maths.PrimeFactorization.pfactors(100);
+        java.lang.Class<?> wildcardClass2 = intList1.getClass();
+        org.junit.Assert.assertNotNull(intList1);
+        org.junit.Assert.assertNotNull(wildcardClass2);
+    }
+
+    @Test
+    public void test14() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test14");
+        java.util.List<java.lang.Integer> intList1 = com.thealgorithms.maths.PrimeFactorization.pfactors((int) (byte) -1);
+        java.lang.Class<?> wildcardClass2 = intList1.getClass();
+        org.junit.Assert.assertNotNull(intList1);
+        org.junit.Assert.assertNotNull(wildcardClass2);
+    }
+
+    @Test
+    public void test15() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test15");
+        java.util.List<java.lang.Integer> intList1 = com.thealgorithms.maths.PrimeFactorization.pfactors((int) (byte) 1);
+        java.lang.Class<?> wildcardClass2 = intList1.getClass();
+        org.junit.Assert.assertNotNull(intList1);
+        org.junit.Assert.assertNotNull(wildcardClass2);
+    }
+
+    @Test
+    public void test16() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test16");
+        java.util.List<java.lang.Integer> intList1 = com.thealgorithms.maths.PrimeFactorization.pfactors((int) (short) 10);
+        java.lang.Class<?> wildcardClass2 = intList1.getClass();
+        org.junit.Assert.assertNotNull(intList1);
+        org.junit.Assert.assertNotNull(wildcardClass2);
+    }
+
+    @Test
+    public void test17() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test17");
+        java.util.List<java.lang.Integer> intList1 = com.thealgorithms.maths.PrimeFactorization.pfactors((int) (short) 100);
+        java.lang.Class<?> wildcardClass2 = intList1.getClass();
+        org.junit.Assert.assertNotNull(intList1);
+        org.junit.Assert.assertNotNull(wildcardClass2);
+    }
+
+    @Test
+    public void test18() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test18");
+        java.util.List<java.lang.Integer> intList1 = com.thealgorithms.maths.PrimeFactorization.pfactors((int) (short) 0);
+        java.lang.Class<?> wildcardClass2 = intList1.getClass();
+        org.junit.Assert.assertNotNull(intList1);
+        org.junit.Assert.assertNotNull(wildcardClass2);
+    }
+
+    @Test
+    public void test19() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test19");
+        java.util.List<java.lang.Integer> intList1 = com.thealgorithms.maths.PrimeFactorization.pfactors((int) ' ');
+        java.lang.Class<?> wildcardClass2 = intList1.getClass();
+        org.junit.Assert.assertNotNull(intList1);
+        org.junit.Assert.assertNotNull(wildcardClass2);
+    }
+
+    @Test
+    public void test20() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test20");
+        java.util.List<java.lang.Integer> intList1 = com.thealgorithms.maths.PrimeFactorization.pfactors((int) (short) -1);
+        java.lang.Class<?> wildcardClass2 = intList1.getClass();
+        org.junit.Assert.assertNotNull(intList1);
+        org.junit.Assert.assertNotNull(wildcardClass2);
+    }
+
+    @Test
+    public void test21() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test21");
+        java.util.List<java.lang.Integer> intList1 = com.thealgorithms.maths.PrimeFactorization.pfactors(0);
+        java.lang.Class<?> wildcardClass2 = intList1.getClass();
+        org.junit.Assert.assertNotNull(intList1);
+        org.junit.Assert.assertNotNull(wildcardClass2);
+    }
+}
+
